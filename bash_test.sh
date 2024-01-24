f93e4b0003a8df16048f870150498df49120142d
vphnguyen <ngcamau@gmail.com>
2024-01-24 11:22:32 +0700

    Done

--- bash_test.sh
+++ bash_test.sh
@@ -1,11 +1,25 @@
 
 # Generate a list of valid values using the ls command
+is_valid_ip() {
+    local ip=$1
+    local regex="^([0-9]{1,3}\.){3}[0-9]{1,3}$"
+
+    if [[ $ip =~ $regex ]]; then
+        return 0  # Valid IP address
+    else
+        return 1  # Invalid IP address
+    fi
+}
 
 while true; do
-    echo  && echo "[     NETWORK INTERFACE     ]" && echo
-    valid_values=($(ls /sys/class/net/))
+
+  # ======================================== INTERFACE ========================================
+  while true; do
+    echo  && echo "NETWORK INTERFACE" 
+    echo "----------"
 
     # Display all valid values with indexes
+    valid_values=($(ls /sys/class/net/))
     echo "List of all interfaces:"
     for index in "${!valid_values[@]}"; do
         echo "$index: ${valid_values[index]}"
@@ -13,113 +27,118 @@ while true; do
     echo "----------"
 
     # Request user input for index of variable 1
-    read -p "Enter the index for 1G interface 1: " index_C1G1
+    read -p "Card  1G - 1: " index_C1G1
     C1G1="${valid_values[index_C1G1]}"
 
     # Request user input for index of variable 2
-    read -p "Enter the index for 1G interface 2: " index_C1G2
+    read -p "Card  1G - 2: " index_C1G2
     C1G2="${valid_values[index_C1G2]}"
 
     echo ""
     # Request user input for index of variable 3
-    read -p "Enter the index for 10G interface 1: " index_C10G1
+    read -p "Card 10G - 1: " index_C10G1
     C10G1="${valid_values[index_C10G1]}"
 
     # Request user input for index of variable 4
-    read -p "Enter the index for 10G interface 2: " index_C10G2
+    read -p "Card 10G - 2: " index_C10G2
     C10G2="${valid_values[index_C10G2]}"
-
-    echo "----------"
-    # Display entered variables
-    echo "You entered:"
-    echo "- 1G interface 1: $C1G1"
-    echo "- 1G interface 2: $C1G2"
-    echo "- 10G interface 1: $C10G1"
-    echo "- 10G interface 2: $C10G2"
 
     # Check if entered variables are in the valid list
     if [[ " ${valid_values[@]} " =~ " $C1G1 " && " ${valid_values[@]} " =~ " $C1G2 " && " ${valid_values[@]} " =~ " $C10G1 " && " ${valid_values[@]} " =~ " $C10G2 " ]]; then
-        echo -e "\nAll variables are in the valid list."
+        echo "----------"
+        echo "All index are in the valid list."
+        echo "----------"
+        break
     else
         echo "----------"
-        echo "Please enter valid values for all variables. Retry."
+        echo "Please enter valid index for all card. Retry."
         echo "----------"
         continue
     fi
+  done
 
-    echo "----------"
-    # Ask for confirmation
-    read -p "Are these correct? (yes/no): " confirmation
 
-    # Check if the user entered 'yes' to exit the loop
-    if [ "$confirmation" = "yes" ]; then
-        echo "Confirmation received. Continue."
-        break
-    else
-        echo "Please try again."
-        echo "----------"
-    fi
-done
 
-while true; do
-    echo "==============="
-    echo && echo  "[     IP ADDRESS     ]" && echo 
+  while true; do
+    echo && echo  "IP ADDRESS"
     read -p "Give us IP address for this server: " ip_address
-    # Ask for confirmation
-    echo "----------"
-    read -p "Are these correct? (yes/no): " confirm
-    if [[ $confirm == "yes" ]]; then
-        echo "Confirmation received. Continue."
+    if is_valid_ip "$ip_address"; then
+        echo "----------"
+        echo  "IP address is valid."
+        echo "----------"
         break
     else
-        echo "Please try again."
         echo "----------"
+        echo "Please enter valid IP address. Retry."
+        echo "----------"
+        continue
     fi
-done
+  done
 
 
-while true; do
-    echo "==============="
-    echo && echo "[     SUBNET PREFIX     ]" && echo 
-    read -p "How about the subnet prefix: " subnet_prefix
-    if [[ $subnet_prefix =~ ^[0-9]+$ ]] && [ $subnet_prefix -ge 0 ] && [ $subnet_prefix -le 32 ]; then
-        echo "Subnet prefix is valid: $subnet_prefix"
-        break
+  while true; do
+      echo && echo "SUBNET PREFIX" 
+      read -p "How about the subnet prefix: " subnet_prefix
+      if [[ $subnet_prefix =~ ^[0-9]+$ ]] && [ $subnet_prefix -ge 0 ] && [ $subnet_prefix -le 32 ]; then
+          echo "----------"
+          echo "Subnet prefix is valid: $subnet_prefix"
+          echo "----------"
+          break
+      else
+          echo "----------"
+          echo "Please enter valid subnet prefix. Retry."
+          echo "----------"
+      fi
+  done
+
+  gateway=''
+  subnet_bit=$((0xFFFFFFFF << (32 - subnet_prefix)))
+  subnet_mask=$((subnet_bit >> 24 & 255)).$((subnet_bit >> 16 & 255)).$((subnet_bit >> 8 & 255)).$((subnet_bit & 255))
+  IFS=. read -r -a ip_parts <<< "$ip_address"
+  IFS=. read -r -a subnet_parts <<< "$subnet_mask"
+  for ((i=0; i<4; i++)); do
+    if [ $i -eq 3 ]; then    
+      gateway_part=$((ip_parts[i] &( subnet_parts[i] )  | 1 ))
     else
-        echo "Please enter valid subnet prefix. Retry."
-        echo "----------"
+      gateway_part=$((ip_parts[i] & subnet_parts[i]))
     fi
-    read -p "Are these correct? (yes/no): " 
-    echo "----------"
-    if [[ $confirm == "yes" ]]; then
-        echo "Confirmation received. Continue."
-        break
-    else
-        echo "Please try again."
-        echo "----------"
+    gateway+="$gateway_part"
+    if [ $i -lt 3 ]; then
+      gateway+="."
     fi
-done
-
-
-gateway=''
-subnet_bit=$((0xFFFFFFFF << (32 - subnet_prefix)))
-subnet_prefix=$((subnet_bit >> 24 & 255)).$((subnet_bit >> 16 & 255)).$((subnet_bit >> 8 & 255)).$((subnet_bit & 255))
-IFS=. read -r -a ip_parts <<< "$ip_address"
-IFS=. read -r -a subnet_parts <<< "$subnet_prefix"
-for ((i=0; i<4; i++)); do
-  if [ $i -eq 3 ]; then    
-    gateway_part=$((ip_parts[i] &( subnet_parts[i] )  | 1 ))
+  done
+  echo && echo "SUMMARIZE"
+  echo "----------"
+  # Display entered variables
+  echo "You entered:"
+  echo "- 1G interface 1: $C1G1"
+  echo "- 1G interface 2: $C1G2" && echo
+  echo "- 10G interface 1: $C10G1"
+  echo "- 10G interface 2: $C10G2"&& echo
+  echo "- IP address: $ip_address/$subnet_prefix"
+  echo "- Subnet mask: $subnet_mask"
+  echo "- Gateway: $gateway"
+  echo "----------"
+  # ======================================== CONFIRM ========================================
+  # Ask for confirmation
+  echo && echo "[     CONFIRM     ]" 
+  echo "----------"
+  read -p "Are these correct? (yes/no): " confirmation
+  # Check if the user entered 'yes' to exit the loop
+  if [ "$confirmation" = "yes" ]; then
+      echo "Confirmation received. Continue."
+      echo "----------"
+      break
   else
-    gateway_part=$((ip_parts[i] & subnet_parts[i]))
-  fi
-  gateway+="$gateway_part"
-  if [ $i -lt 3 ]; then
-    gateway+="."
+      echo "----------"
+      echo "Please try again."
+      echo "----------"
   fi
 done
 
 
-echo " SUMMARIZE "
-echo "- 10G interface 1: $ip_address"
-echo "- 10G interface 1: $subnet_prefix"
-echo "- 10G interface 2: $gateway"
+
+
+
+
+
